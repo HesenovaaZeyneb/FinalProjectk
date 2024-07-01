4e32a4e4098531a7c2c410feceb3a92838d9ee10
HesenovaaZeyneb <tu7wsdwla@code.edu.az>
2024-07-01 17:56:28 +0400

    update

--- GoldenGate hospital/Evergreen_Domain/obj/Debug/net8.0/Evergreen_Domain.AssemblyInfo.cs
+++ GoldenGate hospital/Evergreen_Domain/obj/Debug/net8.0/Evergreen_Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Evergreen_Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d3accfdde66fcfd6f0263720a43dd9ce02b117")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d945cc6a77c00b6179726cc760a682fc3c72b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Evergreen_Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Evergreen_Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
